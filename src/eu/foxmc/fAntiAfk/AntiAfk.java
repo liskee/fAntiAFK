33225198998791febbfa26010384d7f26edf669a
liskee <jan.opala@yahoo.com>
2021-11-25 16:02:27 +0100

    test

---
+++ src/eu/foxmc/fAntiAfk/AntiAfk.java
@@ -0,0 +1,11 @@
+package eu.foxmc.fAntiAfk;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class AntiAfk implements Listener {
+    @EventHandler
+    public void AntiAfkEvent(){
+        
+    }
+}
---
+++ src/eu/foxmc/fAntiAfk/Main.java
@@ -0,0 +1,22 @@
+package eu.foxmc.fAntiAfk;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new AntiAfk(), this);
+        loadConfig();
+    }
+
+    public void onDisable() {
+
+    }
+
+    public void loadConfig() {
+        // Time to kick for afk in minutes
+        getConfig().addDefault("afk-time", 1);
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+}
